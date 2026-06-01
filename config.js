9c59b90b472b0e1ea4f58d536bef15214d386ac1
R3hab228 <mtyagniryadno@gmail.com>
2026-06-01 21:39:22 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.ENV = {
-    SUPABASE_URL: "https://nmpuefxqtkhvtltdvllz.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tcHVlZnhxdGtodnRsdGR2bGx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MzQ2MzEsImV4cCI6MjA5NDUxMDYzMX0.SPspCdwRKd6Hu0Zx3Hhm-orTOpPdcigNWLiV1TTwLMo"
-};
