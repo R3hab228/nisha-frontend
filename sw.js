df1db153e881cd1d96c7a6c0df1ad51f27879881
R3hab228 <mtyagniryadno@gmail.com>
2026-06-02 21:06:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nisha-cache-v38'; 
+const CACHE_NAME = 'nisha-cache-v39'; 
 const STATIC_URLS = ['/', '/index.html', '/app.js', '/config.js', '/style.css', '/locales.json'];
 
 self.addEventListener('install', event => {
