4903b0bcaaa9ef48d2b77cae383a7255a0360b33
R3hab228 <mtyagniryadno@gmail.com>
2026-06-09 21:58:59 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nisha-cache-v72'; 
+const CACHE_NAME = 'nisha-cache-v73'; 
 const STATIC_URLS = ['/', '/index.html', '/app.js', '/config.js', '/style.css', '/locales.json'];
 
 self.addEventListener('install', event => {
