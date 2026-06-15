ebe7eb27192dcd0caa5b536c17e8f42c8d594d84
R3hab228 <mtyagniryadno@gmail.com>
2026-06-15 14:13:10 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 window.ENV = {
     SUPABASE_URL: "https://nmpuefxqtkhvtltdvllz.supabase.co",
     SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tcHVlZnhxdGtodnRsdGR2bGx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5MzQ2MzEsImV4cCI6MjA5NDUxMDYzMX0.SPspCdwRKd6Hu0Zx3Hhm-orTOpPdcigNWLiV1TTwLMo",
-    RECAPTCHA_SITE_KEY: "6LcGyQctAAAAAN9-z_X53liU0VhxomIrXAxv4DdP"
 };
